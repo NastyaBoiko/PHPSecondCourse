45abc0f337d42e53898175cf66566f96a63ad706
Nastya <killer99whale@mail.ru>
2024-01-26 14:35:34 +0300

    7 done

--- practice2/6LessonClasses7.php
+++ practice2/6LessonClasses7.php
@@ -1,4 +1,58 @@
 <?php
+$users = json_decode(file_get_contents('users.json'));
+
+if (isset($_POST['type'])) {
+    array_push($users, $_POST);
+    file_put_contents('users.json', json_encode($users, JSON_UNESCAPED_UNICODE));
+    header("Location: 6LessonClasses7.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users</title>
+</head>
+<body>
+    <h1>Форма добавления студента</h1>
+    <form action="#" method="post">
+        <input type="hidden" name="type" value="Student"/>
+
+        <label for="firstName">Имя:</label>
+        <input name="firstName" id="firstName" type="text" required>
+        <br>
+        <br>
+
+        <label for="lastName">Фамилия:</label>
+        <input name="lastName" id="lastName" type="text" required>
+        <br>
+        <br>
+
+        <label for="email">Email:</label>
+        <input name="email" id="email" type="text" required>
+        <br>
+        <br>
+
+        <label for="cource">Курс:</label>
+        <input name="cource" id="cource" type="number" required>
+        <br>
+        <br>
+
+        <label for="groupe">Группа:</label>
+        <input name="groupe" id="groupe" type="text" required>
+        <br>
+        <br>
+
+
+        <button type="submit">Добавить</button>
+        <!-- ["type" => "Student", "firstName" => "Bob", "lastName" => "Nossom", "email" => "Bob@mail.com", "cource" => 4, "groupe" => "ИВ2-22"], -->
+    </form>
+</body>
+</html>
+
+<?php
 
 class User {
     private $firstName, $lastName, $email, $role = "User";
@@ -148,19 +202,17 @@ class Manager extends User {
     }
 }
 
-$persons = [
-    ["type" => "User", "firstName" => "Tommy", "lastName" => "Marley", "email" => "Marley@mail.com"],
-    ["type" => "Student", "firstName" => "Bob", "lastName" => "Nossom", "email" => "Bob@mail.com", "cource" => 4, "groupe" => "ИВ2-22"],
-    ["type" => "Student", "firstName" => "Alice", "lastName" => "Berry", "email" => "Berry@mail.com", "cource" => 3, "groupe" => "ИВ2-21"],
-    ["type" => "Teacher", "firstName" => "Mary", "lastName" => "Lock", "email" => "Lock@mail.com", "subjects" => ['русский', 'литература']],
-    ["type" => "Teacher", "firstName" => "Josh", "lastName" => "Washington", "email" => "Washington@mail.com", "subjects" => ['английский']],
-    ["type" => "Manager", "firstName" => "Lenny", "lastName" => "Lockster", "email" => "Lockster@mail.com", "position" => "завуч", "jobDuties" => ['организация учебного процесса', 'выполнение учебных программ']],
-    ["type" => "Manager", "firstName" => "Helen", "lastName" => "Marine", "email" => "Marine@mail.com", "position" => "директор", "jobDuties" => ['организация образовательной (учебно-воспитательной) работы', ' создание режима соблюдения норм и правил']]
-];
-
-file_put_contents('users.json', json_encode($persons, JSON_UNESCAPED_UNICODE));
-
-$users = json_decode(file_get_contents('users.json'));
+// Для быстрого создания json
+// $persons = [
+//     ["type" => "User", "firstName" => "Tommy", "lastName" => "Marley", "email" => "Marley@mail.com"],
+//     ["type" => "Student", "firstName" => "Bob", "lastName" => "Nossom", "email" => "Bob@mail.com", "cource" => 4, "groupe" => "ИВ2-22"],
+//     ["type" => "Student", "firstName" => "Alice", "lastName" => "Berry", "email" => "Berry@mail.com", "cource" => 3, "groupe" => "ИВ2-21"],
+//     ["type" => "Teacher", "firstName" => "Mary", "lastName" => "Lock", "email" => "Lock@mail.com", "subjects" => ['русский', 'литература']],
+//     ["type" => "Teacher", "firstName" => "Josh", "lastName" => "Washington", "email" => "Washington@mail.com", "subjects" => ['английский']],
+//     ["type" => "Manager", "firstName" => "Lenny", "lastName" => "Lockster", "email" => "Lockster@mail.com", "position" => "завуч", "jobDuties" => ['организация учебного процесса', 'выполнение учебных программ']],
+//     ["type" => "Manager", "firstName" => "Helen", "lastName" => "Marine", "email" => "Marine@mail.com", "position" => "директор", "jobDuties" => ['организация образовательной (учебно-воспитательной) работы', ' создание режима соблюдения норм и правил']]
+// ];
+// file_put_contents('users.json', json_encode($persons, JSON_UNESCAPED_UNICODE));
 
 $objUsers = [];
 
@@ -173,6 +225,4 @@ foreach($users as $user) {
     array_push($objUsers, $objUser);
 }
 
-foreach ($objUsers as $obj) {
-    $obj->sayAboutMe();
-}
+array_map(fn($obj) => $obj->sayAboutMe(), $objUsers);
