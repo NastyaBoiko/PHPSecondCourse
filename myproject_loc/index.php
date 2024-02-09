633c88f6fa4db68216f2e712a61296fba85127cd
nastya <killer99whale@mail.ru>
2024-02-09 14:12:40 +0300

    start

--- myproject_loc/index.php
+++ myproject_loc/index.php
@@ -1,5 +1,41 @@
 <?php
 
+spl_autoload_register();
+
 $route = $_GET['route'] ?? '';
 
-echo 'route ' . $route;
+// echo 'route ' . $route;
+
+$pattern = '~^hello/(.*)$~';
+
+preg_match($pattern, $route, $matches);
+
+if (!empty($matches)) {
+    $controller = new \Src\Controllers\MainController();
+    $controller->sayHello($matches[1]);
+    return;
+}
+
+$pattern = "~^$~";
+
+preg_match($pattern, $route, $matches);
+
+if (!empty($matches)) {
+    $controller = new \Src\Controllers\MainController();
+    $controller->main();
+    return;
+}
+
+$pattern = '~^echo/(.*)$~';
+
+preg_match($pattern, $route, $matches);
+
+if (!empty($matches)) {
+    $controller = new \Src\Controllers\MainController();
+    $controller->len($matches[1]);
+    return;
+}
+
+
+// $controller = new \Src\Controllers\MainController();
+// $controller->main();
